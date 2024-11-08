099de319ec9dc99b9392fd8c4a3bbc28d77f7c2c
Yasser Messahli <yassermessahli0@gmail.com>
2024-11-08 09:55:43 +0100

    small changes

--- app/config.py
+++ app/config.py
@@ -1,24 +1,24 @@
 # config.py for competition configuration
 # used by app.py
 
-COMPETITION_NAME = "Mini Datathon - SOAI"
+COMPETITION_NAME = ""
 
-START_TIME = "2024-11-07T19:00:00+00:00"
-END_TIME = "2024-11-08T18:00:00+00:00"
+START_TIME = "0000-00-00T00:00:00+00:00"
+END_TIME = "0000-00-00T00:00:00+00:00"
 
 TEAMS = [
-    "kurtosis",
-    "Vgaith",
-    "Megatron"
+    "",
+    "",
+    ""
 ]
 
 COMPETITIONS = [
-    "pcbm-challge",
-    "jumia-purchase-prediction",
-    "instadeep-challenge-soai"
+    "",
+    "",
+    ""
 ]
 
-WEIGHTS = [40, 35, 25]
+WEIGHTS = []
 
 
 # ==================================================================================
