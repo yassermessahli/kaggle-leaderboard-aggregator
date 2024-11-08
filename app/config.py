198b823ca82bf67b7fdaffcfdbfa8098e3a81e54
Yasser Messahli <yassermessahli0@gmail.com>
2024-11-08 10:00:58 +0100

    add screenshot for the leaderboard

--- app/config.py
+++ app/config.py
@@ -1,24 +1,27 @@
 # config.py for competition configuration
 # used by app.py
 
-COMPETITION_NAME = ""
+COMPETITION_NAME = "Mini Datathon - SOAI"
 
-START_TIME = "0000-00-00T00:00:00+00:00"
-END_TIME = "0000-00-00T00:00:00+00:00"
+START_TIME = "2024-11-07T19:00:00+00:00"
+END_TIME = "2024-11-08T18:00:00+00:00"
 
 TEAMS = [
-    "",
-    "",
-    ""
+    "kurtosis",
+    "Vgaith",
+    "Megatron",
+    "GenTech.io",
+    "Trainers",
+    "Pounders"
 ]
 
 COMPETITIONS = [
-    "",
-    "",
-    ""
+    "pcbm-challenge",
+    "jumia-purchase-prediction",
+    "instadeep-challenge-soai"
 ]
 
-WEIGHTS = []
+WEIGHTS = [40, 35, 25]
 
 
 # ==================================================================================
